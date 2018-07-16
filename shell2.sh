c8086b7e729b068875b88a50481b40f54bd84355
Utsav B. Shah <shahutsavab19596@gmail.com>
2018-07-16 12:04:24 +0530

    shell assignment problem 2
    2) Given two integers, x and y , find their sum, difference, product, and quotient.

---
+++ shell2.sh
@@ -0,0 +1,13 @@
+x=10
+y=2
+expr $x + $y #addition of variables
+expr $x - $y #subtraction of variables
+expr $x * $y #Multiplication of variables
+expr $x / $y #Quotient of variable
+
+echo -e "\nGive integer inputs for x & y: "
+read x y
+echo "Addition is :" `expr $x + $y` #addition of variables
+echo "Subtraction is:" `expr $x - $y` #subtraction of variables
+echo "Multiplication is: " `expr $x * $y` #Multiplication of variables
+echo "Quotient is: " `expr $x / $y` #Quotient of variable
